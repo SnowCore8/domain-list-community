8d1369334c7085508577c82131202d2aede1f342
SnowCore8 <snowcore8@gmail.com>
2025-12-26 01:47:30 +0800

    config: 修剪配置

---
+++ scripts/get.sh
@@ -0,0 +1,10 @@
+for i in facebook geolocation-!cn google netflix private telegram twitter
+do
+  echo "- Download $i"
+  curl -sSL https://raw.githubusercontent.com/MetaCubeX/meta-rules-dat/meta/geo/geosite/${i}.list |sed "s/\+\.//g" |sed "s/^/full:/g" > data/${i}
+done
+
+echo "- Download cn"
+curl -sSL https://raw.githubusercontent.com/SnowCore8/IPChinaList/master/china_domain.txt |sed "s/\+\.//g" |sed "s/^/full:/g" > data/cn
+echo "- Download gfw"
+curl -sSL https://raw.githubusercontent.com/SnowCore8/IPChinaList/master/gfw_domain.txt |sed "s/\+\.//g" |sed "s/^/full:/g" > data/gfw
